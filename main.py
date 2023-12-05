0f445adc690bf83c97262ac2df5f1ce875531912
Peyam Ekenel <peyamekenel@hotmail.com>
2023-12-05 11:32:50 +0300

    shrinking square

--- main.py
+++ main.py
@@ -3,21 +3,16 @@ drawing_board=turtle.Screen()
 drawing_board.bgcolor("purple")
 drawing_board.title("python turttle")
 turtle_instance=turtle.Turtle()
+turtle_instance.color("yellow")
 
-for i in range(4):
-    turtle_instance.forward(200)
-    turtle_instance.right(90)
+def shrinkingSquare(size):
+    for i in range(4):
+        turtle_instance.forward(size)
+        turtle_instance.left(90)
+        size=size-5
 
-turtle_instance2=turtle.Turtle()
-for i in range(5):
-    turtle_instance.back(250)
-    turtle_instance.left(144)
-
-turtle_instance3=turtle.Turtle()
-num_sides=8
-angle=360/num_sides
-side_length=100
-for i in range(num_sides):
-    turtle_instance.forward(side_length)
-    turtle_instance.left(angle)
+x=300
+while x>0:
+    shrinkingSquare(x)
+    x=x-20
 turtle.done()
