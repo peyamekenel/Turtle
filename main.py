8072f8db475e0716f213a85c399cacff0021c774
Peyam Ekenel <peyamekenel@hotmail.com>
2023-12-05 14:00:20 +0300

    circle revision

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import turtle
 drawing_board=turtle.Screen()
-drawing_board.bgcolor("purple")
+drawing_board.bgcolor("white")
 drawing_board.title("python turttle")
 turtle_instance=turtle.Turtle()
 turtle_instance.color("yellow")
-
-def shrinkingSquare(size):
-    for i in range(4):
-        turtle_instance.forward(size)
-        turtle_instance.left(90)
-        size=size-5
-
-x=300
-while x>0:
-    shrinkingSquare(x)
-    x=x-20
-turtle.done()
+turtle.speed(0)
+colors_List= ["red","blue","pink","green","orange","brown"]
+for i in range(20):
+    turtle_instance.color(colors_List[i%6])
+    turtle_instance.circle(10*i)
+    turtle_instance.circle(-10*i)
+    turtle_instance.left(i)
+turtle.mainloop()
