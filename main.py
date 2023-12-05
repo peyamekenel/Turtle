b3f00f595b41baa422b9f5722a901dbcaaea654b
Peyam Ekenel <peyamekenel@hotmail.com>
2023-12-05 10:34:37 +0300

    turtle drawing square

---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle
+drawing_board=turtle.Screen()
+drawing_board.bgcolor("purple")
+drawing_board.title("python turttle")
+turtle_instance=turtle.Turtle()
+
+turtle_instance.forward(200)
+turtle_instance.right(90)
+turtle_instance.forward(200)
+turtle_instance.right(90)
+turtle_instance.forward(200)
+turtle_instance.right(90)
+turtle_instance.forward(200)
+turtle.done()
