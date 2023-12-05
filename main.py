58093c63db9cbd5993b24711e1ad999f8a4cc2b1
Peyam Ekenel <peyamekenel@hotmail.com>
2023-12-05 10:56:03 +0300

    added star and polygon

--- main.py
+++ main.py
@@ -7,4 +7,17 @@ turtle_instance=turtle.Turtle()
 for i in range(4):
     turtle_instance.forward(200)
     turtle_instance.right(90)
+
+turtle_instance2=turtle.Turtle()
+for i in range(5):
+    turtle_instance.back(250)
+    turtle_instance.left(144)
+
+turtle_instance3=turtle.Turtle()
+num_sides=8
+angle=360/num_sides
+side_length=100
+for i in range(num_sides):
+    turtle_instance.forward(side_length)
+    turtle_instance.left(angle)
 turtle.done()
