f5609f2655298ae1242ef84ba00f6a66a14b92f4
Peyam Ekenel <peyamekenel@hotmail.com>
2023-12-06 14:56:31 +0300

    Turtle Interactive

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import turtle
 drawing_board=turtle.Screen()
-drawing_board.bgcolor("white")
-drawing_board.title("python turttle")
+drawing_board.bgcolor("light blue")
+drawing_board.title("python turtle")
 turtle_instance=turtle.Turtle()
-turtle_instance.color("yellow")
-turtle.speed(0)
-colors_List= ["red","blue","pink","green","orange","brown"]
-for i in range(20):
-    turtle_instance.color(colors_List[i%6])
-    turtle_instance.circle(10*i)
-    turtle_instance.circle(-10*i)
-    turtle_instance.left(i)
+turtle_instance.color("purple")
+def turtle_forward():
+    turtle_instance.forward(100)
+def rotate_angle_right():
+    turtle_instance.setheading(turtle_instance.heading()-10)
+
+def rotate_angle_left():
+    turtle_instance.setheading(turtle_instance.heading()+10)
+def clear_screen():
+    turtle_instance.clear()
+def turtle_return_home():
+    turtle_instance.penup()
+    turtle_instance.home()
+    turtle_instance.pendown()
+def turtle_pen_up():
+    turtle_instance.penup()
+def turtle_pen_down():
+    turtle_instance.pendown()
+
+drawing_board.listen()
+drawing_board.onkey(fun=turtle_forward,key="space")
+drawing_board.onkey(fun=rotate_angle_right,key="Down")
+drawing_board.onkey(fun=rotate_angle_left,key="Up")
+drawing_board.onkey(fun=clear_screen,key="c")
+drawing_board.onkey(fun=turtle_pen_up,key="w")
+drawing_board.onkey(fun=turtle_pen_down,key="q")
+drawing_board.onkey(fun=turtle_return_home,key="h")
 turtle.mainloop()
